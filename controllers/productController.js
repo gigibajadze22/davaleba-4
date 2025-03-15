4a09671ada07eb4326b3959360af2c77b7be923a
Gigi bajadze <you@example.com>
2025-03-15 21:40:09 +0400

    davaleba 4

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,8 +1,25 @@
 import Product from "../models/productModel.js"
 
 const getProducts = async (req,res) =>{
+  const excludeFields = ['sort','fields','page','limit']
+  const queryObj = {...req.query}
+
+  console.log(req.query);
   try{
-    const product = await Product.find({name:'Product 4'}) 
+  let query = Product.find() 
+ excludeFields.forEach(el =>delete queryObj[el])
+    query = query.find(queryObj) 
+   if(req.query.sort) query = query.sort(req.query.sort)
+   if(req.query.fields) query= query.select(req.query.fields.split(',').join(' '))
+
+
+    const page = req.query.page * 1 || 1
+    const limit = req.query.limit *1 ||100
+    const skip = (page-1) * limit
+    query = query.skip(skip).limit(limit)
+
+
+    const product = await query
   res.json(product)
   }catch(error) {
     res.status(400).json({message: error.message})
--- models/productModel.js
+++ models/productModel.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const productsSchema = new mongoose.Schema({
     id: { type: Number, required: true, unique: true },
     name: { type: String, required: [true, "Name is Required"] },
-    price: { type: Number, required: true },
+    price: { type: Number, required: true, validate: {validator: (val) => val >0, message: "price must be greater than 0" }},
     stock: { type: Number, required: true },
     createdAt: { type: Date, default: Date.now }
 });
